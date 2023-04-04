a1eb91b858392e9d5a2c0f6bf86f5f262e13a662
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-04-03 23:35:01 -0300

    :sparkles: feat: add equals and hash code to Filme class

--- TestesUnitarios/src/main/java/br/ce/wcaquino/entidades/Filme.java
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/entidades/Filme.java
@@ -32,4 +32,43 @@ public class Filme {
 	public void setPrecoLocacao(Double precoLocacao) {
 		this.precoLocacao = precoLocacao;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((estoque == null) ? 0 : estoque.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((precoLocacao == null) ? 0 : precoLocacao.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Filme other = (Filme) obj;
+		if (estoque == null) {
+			if (other.estoque != null)
+				return false;
+		} else if (!estoque.equals(other.estoque))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (precoLocacao == null) {
+			if (other.precoLocacao != null)
+				return false;
+		} else if (!precoLocacao.equals(other.precoLocacao))
+			return false;
+		return true;
+	}
+	
+	
 }
