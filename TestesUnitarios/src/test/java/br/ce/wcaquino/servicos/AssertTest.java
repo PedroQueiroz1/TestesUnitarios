7719a264ca466a58c3ef0ba6738f28a3d404aa77
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-04-03 23:38:53 -0300

    :white_check_mark: test: Add new tests.

--- TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/AssertTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/AssertTest.java
@@ -13,6 +13,7 @@ public class AssertTest {
 		Assert.assertTrue(true);
 		Assert.assertFalse(false);
 		
+		Assert.assertEquals("Erro de comparacao", 1, 2);
 		Assert.assertEquals(1, 2);
 		Assert.assertEquals(0.51234, 0.512, 0.0001);
 		Assert.assertEquals(Math.PI, 3.14, 0.01);
@@ -23,12 +24,20 @@ public class AssertTest {
 		Assert.assertEquals(Integer.valueOf(i), i2);
 		
 		Assert.assertEquals("bola", "bola");
+		Assert.assertNotEquals("bola", "boola");
 		Assert.assertTrue("bola".equalsIgnoreCase("Bola"));
 		Assert.assertTrue("bola".startsWith("bo"));
 		
 		Usuario u1 = new Usuario("Carlos");
 		Usuario u2 = new Usuario("Marcos");
+		Usuario u3 = u2;
+		Usuario u4 = null;
 		
 		Assert.assertEquals(u1, u2);
+		Assert.assertSame(u2, u2);
+		Assert.assertNotSame(u1, u2);
+		Assert.assertTrue(u4 == null);
+		Assert.assertNull(u4);
+		Assert.assertNotNull(u2);
 	}
 }
